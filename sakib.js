55e4c0e8da72312eb45e967e1acb7960f1872aa9
Sakib Fakir <fakirsakib22232>
2024-09-05 01:16:38 +0600

    menu button add

--- sakib.js
+++ sakib.js
@@ -14,7 +14,7 @@ const vowelCounter = (text) => {
     let counter = 0; /// this is counter count every char
 
 
-    
+
     for (let i = 0; i < text.length; i++) {
         if (text[i] == 'a' || text[i] == 'A' || text[i] == 'e' || text[i] == 'E' || text[i] == 'i' || text[i] == 'I' || text[i] == 'o' || text[i] == 'O' || text[i] == 'e' || text[i] == 'E') {
             counter++;
@@ -31,4 +31,9 @@ button.addEventListener("click",()=>{
     vowelCounter(input.value);
     console.log(input.value);
     console.log("vowel counter tiggred")
+})
+
+let menu = document.getElementById("menu");
+menu.addEventListener("click",()=>{
+    alert('No More')
 })
